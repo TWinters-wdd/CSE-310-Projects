61e03f86d7c7720c80343e5197775a83b414f0af
HotGarbo04 <talmageawinters@gmail.com>
2026-09-18 12:12:44 -0600

    Hello World Project Changes

--- HelloWorld/obj/Debug/net10.0/HelloWorld.AssemblyInfo.cs
+++ HelloWorld/obj/Debug/net10.0/HelloWorld.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("HelloWorld")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1c07ef7075c1075c8ea509460329b14c438e297f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6c56d431af13b470aaf0231ead684c2984588578")]
 [assembly: System.Reflection.AssemblyProductAttribute("HelloWorld")]
 [assembly: System.Reflection.AssemblyTitleAttribute("HelloWorld")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
