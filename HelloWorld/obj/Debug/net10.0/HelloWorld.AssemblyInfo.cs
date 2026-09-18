4b5937053a452eef19b6c1a2a1288a18d43473c1
HotGarbo04 <talmageawinters@gmail.com>
2026-09-18 12:24:37 -0600

    .gitignore updated

--- HelloWorld/obj/Debug/net10.0/HelloWorld.AssemblyInfo.cs
+++ HelloWorld/obj/Debug/net10.0/HelloWorld.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("HelloWorld")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5a3f0ba714b054dc0fc0c2b84cdc442e4434bf53")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e0f445f7e1e4601c010d7d33abb91c74d63437a4")]
 [assembly: System.Reflection.AssemblyProductAttribute("HelloWorld")]
 [assembly: System.Reflection.AssemblyTitleAttribute("HelloWorld")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
