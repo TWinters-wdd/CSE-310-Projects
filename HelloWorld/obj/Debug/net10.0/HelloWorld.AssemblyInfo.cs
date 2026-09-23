d14591473b33efbcacd9f8c6f8ad3b11a74e3f49
HotGarbo04 <talmageawinters@gmail.com>
2026-09-23 14:07:54 -0600

    Basic Shapes Created in Python Arcade

--- HelloWorld/obj/Debug/net10.0/HelloWorld.AssemblyInfo.cs
+++ HelloWorld/obj/Debug/net10.0/HelloWorld.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("HelloWorld")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ed4f71427c351b0f8169f4b7faae6644df16b747")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c7b6855916233b1dd7c11739ca851ba592077511")]
 [assembly: System.Reflection.AssemblyProductAttribute("HelloWorld")]
 [assembly: System.Reflection.AssemblyTitleAttribute("HelloWorld")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
